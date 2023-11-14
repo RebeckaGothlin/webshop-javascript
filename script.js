2bde769cc0a5c12060361786b7ba5c0c88b169a7
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2023-11-14 15:54:08 +0100

    :lipstick: style burger menu

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector('#toggleMenu');
+const menu = document.querySelector('#menu');
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    const isOpen = menu.classList.toggle('open');
+    menuBtn.classList.toggle('open');
+    if (isOpen) {
+        menuBtn.setAttribute('aria-label', 'Close menu');
+    } else {
+        menuBtn.setAttribute('aria-label', 'Open menu');
+    }
+}
