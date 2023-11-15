3b3a3887a09197d9b94962b8543a51feb0e68fc1
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2023-11-15 09:09:34 +0100

    :lipstick: add theme toggle

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ function toggleMenu() {
         menuBtn.setAttribute('aria-label', 'Open menu');
     }
 }
+
+
+if (
+    window.matchMedia && 
+    window.matchMedia('(prefers-color-scheme: dark)').matches) 
+{
+    document.body.classList.add('dark-theme');
+}
+
+
+const themeToggle = document.querySelector("#themeToggle");
+
+themeToggle.addEventListener('click', toggleTheme);
+
+function toggleTheme() {
+    if (document.body.classList.contains('dark-theme')) {
+    document.body.classList.remove('dark-theme');
+    } else {
+    document.body.classList.add('dark-theme');
+    }
+}
